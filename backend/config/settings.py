53640ea350c163f50113d9ab59ea0161387ba35f
T-101 <antti.rummukainen@gmail.com>
2023-09-13 19:15:39 +0300

    add global login redirect urls

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -177,6 +177,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = '/'
+
 STATIC_ROOT = env('STATIC_ROOT')
 STATIC_URL = env('STATIC_URL')
 
